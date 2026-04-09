47a0070a4dcc802c413ba0b199a8e3ec9374e739
Kanchi Pandey <kanchip8125@gmail.com>
2026-04-09 14:31:31 +0530

    Update login_process.php

--- login_process.php
+++ login_process.php
@@ -12,13 +12,16 @@ if ($result->num_rows > 0) {
 
     $user = $result->fetch_assoc();
 
-    // password verify (since you are using hash)
     if (password_verify($password, $user['password'])) {
 
-        // store full user
         $_SESSION['user'] = $user;
+        $_SESSION['user_id'] = $user['id'];
 
-        header("Location: dashboard.php");
+        if ($user['interests_selected'] == 0) {
+            header("Location: interests.php");
+        } else {
+            header("Location: dashboard.php");
+        }
         exit();
 
     } else {
