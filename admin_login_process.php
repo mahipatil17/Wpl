d873511b5b5169b52cbed7fabadded5c00a29982
mahipatil17 <mahi.patil@somaiya.edu>
2026-04-23 13:26:09 +0530

    Refactor admin login process to use prepared statements
    Refactored SQL query to use prepared statements for better security against SQL injection.

--- admin_login_process.php
+++ admin_login_process.php
@@ -5,17 +5,17 @@ session_start();
 $username = $_POST['username'];
 $password = $_POST['password'];
 
-// check admin table
-$sql = "SELECT * FROM admin WHERE username='$username' AND password='$password'";
-$result = $conn->query($sql);
+$sql = "SELECT * FROM admin WHERE username=? AND password=?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("ss", $username, $password);
+$stmt->execute();
+
+$result = $stmt->get_result();
 
 if ($result->num_rows > 0) {
-
     $_SESSION['admin'] = $username;
-
     header("Location: admin_dashboard.php");
     exit();
-
 } else {
     header("Location: admin_login.php?error=1");
     exit();
